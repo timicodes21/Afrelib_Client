2eec11da86c0fac179e9a11dce2e2a21ff2981ea
timicodes21 <timileyinbabalola42@gmail.com>
2023-04-10 13:30:27 +0100

    Updates

--- src/hooks/auth/useUserDetails.ts
+++ src/hooks/auth/useUserDetails.ts
@@ -3,25 +3,36 @@ import { IUserDetails } from "@/types";
 import { useEffect, useState } from "react";
 
 export const useUserDetails = () => {
-  const [userDetails, setUserDetails] = useState<IUserDetails | {}>({
+  const [userDetails, setUserDetails] = useState<IUserDetails>({
     access_token: "",
     first_name: "",
     last_name: "",
     role: "",
   });
 
-  let user: IUserDetails | {} = {};
+  let user: IUserDetails;
 
   useEffect(() => {
-    user = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? "{}");
+    user = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY) ??
+        JSON.stringify({
+          first_name: null,
+          last_name: null,
+          access_token: null,
+          role: null,
+        }),
+    );
 
+    // set userDetails to user in local storagr if present
     setUserDetails(
-      user ?? {
-        first_name: null,
-        last_name: null,
-        access_token: null,
-        role: null,
-      },
+      user?.access_token
+        ? user
+        : {
+            first_name: null,
+            last_name: null,
+            access_token: null,
+            role: null,
+          },
     );
   }, []);
 
