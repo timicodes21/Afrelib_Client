c8d5eb22a2d234173254670e02609ab266f909e5
timicodes21 <timileyinbabalola42@gmail.com>
2023-04-11 15:19:52 +0100

    Authlayout design fixes

--- src/components/organisms/modals/CustomModal.tsx
+++ src/components/organisms/modals/CustomModal.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { SxProps } from "@mui/material";
 import { Modal } from "react-responsive-modal";
 
 interface IProps {
